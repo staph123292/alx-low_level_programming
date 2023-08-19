87c216e5951cf9e7c73dde25c3b069f3fba14608
Mustapha KHALID <mosstaf096@gmail.com>
2023-08-19 00:25:11 -0700

    done

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (!str)
 			str = "(nil)";
 		if (!separator)
-			pritf("%s", str);
+			printf("%s", str);
 		else if (separator && i == 0)
 			printf("%s", str);
 		else
